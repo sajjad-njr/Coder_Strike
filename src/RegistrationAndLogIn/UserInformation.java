a5f8e2086086fe3d21bedb7dbaaff8dc2029f8d7
sajjad-njr <maxsajjad29@gmail.com>
2021-10-03 17:48:02 +0600

    set attribute and method on useinformation class

--- src/RegistrationAndLogIn/UserInformation.java
+++ src/RegistrationAndLogIn/UserInformation.java
@@ -1,4 +1,62 @@
 package RegistrationAndLogIn;
 
+import java.util.Scanner;
+
 public class UserInformation {
+    String firstName;
+    String lastName;
+    String phoneNumber;
+    String email;
+    String setPassword;
+
+    Scanner input = new Scanner(System.in);
+    public UserInformation()
+    {
+
+    }
+    public UserInformation(String lastName,String setPassword)
+    {
+
+        this.lastName = lastName;
+        this.setPassword = setPassword;
+    }
+    public UserInformation(String firstName , String lastName,String phoneNumber,String email , String setPassword)
+    {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+        this.setPassword = setPassword;
+
+    }
+    public  void takeInfo()
+    {
+        System.out.println("Enter your First name : ");
+        firstName = input.nextLine();
+        System.out.println("Enter your Last name : ");
+        lastName = input.nextLine();
+        System.out.println("Enter your Phone Number : ");
+        phoneNumber= input.nextLine();
+        System.out.println("Enter your email id  : ");
+        email = input.nextLine();
+        System.out.println("Set your Password : ");
+         setPassword = input.nextLine();
+
+    }
+
+    public  void showInfo()
+    {
+        System.out.println("***************************************************************");
+        System.out.println("Your First name : "+firstName);
+
+        System.out.println("Your Last name : "+lastName);
+
+        System.out.println("Your Phone Number : "+phoneNumber);
+
+        System.out.println("Your email id  : "+email);
+
+        System.out.println("Your Password : "+setPassword);
+
+        System.out.println("***************************************************************");
+    }
 }
--- src/RegistrationAndLogIn/UserRegistration.java
+++ src/RegistrationAndLogIn/UserRegistration.java
@@ -1,5 +1,11 @@
 package RegistrationAndLogIn;
 
 public class UserRegistration {
+    public static void main(String[] args) {
 
+        UserInformation obj = new UserInformation();
+
+        obj.takeInfo();
+        obj.showInfo();
+    }
 }
